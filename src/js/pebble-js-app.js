840fe2b04cb463de6ff1627456dfe81ae4aa9913
Sebastian Rabuini <sebas@wasabit.com.ar>
2015-01-03 06:35:24 -0300

    Add providers

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,25 +1,30 @@
 function fetchData() {
   var response;
   var req = new XMLHttpRequest();
-  req.open('GET', "https://www.bitstamp.net/api/ticker/");
+
+  var provider = new Bitstamp();
+  console.log(provider.url);
+  req.open('GET', provider.url);
+
   req.onload = function(e) {
     if (req.readyState == 4) {
       if(req.status == 200) {
         response = JSON.parse(req.responseText);
         var price, timestamp;
         if (response) {
-          price = response.last;
-          timestamp = response.timestamp;
+          var data = provider.data(response);
+          price = data.price;
+          timestamp = data.timestamp;
           var date = new Date(timestamp * 1000);
           var hours = date.getHours();
           var minutes = date.getMinutes();
-          minutes = (minutes < 10 ? '0' + minutes : minutes)
+          minutes = (minutes < 10 ? '0' + minutes : minutes);
           timestamp = hours + ':' + minutes;
           console.log(price);
           console.log(timestamp);
           Pebble.sendAppMessage({
             "price":'$' + price,
-            "timestamp": timestamp
+            "timestamp": provider.name + ':' + timestamp
           });
         }
       } else {
@@ -28,6 +33,24 @@ function fetchData() {
     }
   };
   req.send(null);
+}
+
+function BtcE(){
+  this.url = "https://btc-e.com/api/3/ticker/btc_usd";
+  this.name = "BTC-e";
+  
+  this.data = function(response){
+    return {price: response.btc_usd.last, timestamp: response.btc_usd.updated};
+  };
+}
+
+function Bitstamp(){
+  this.url = "https://www.bitstamp.net/api/ticker/";
+  this.name = "Bitstamp";
+  
+  this.data = function(response) {
+    return {price: response.last, timestamp: response.timestamp};
+  };
 }
 
 Pebble.addEventListener("ready",
