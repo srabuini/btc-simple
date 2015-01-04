b55df5d62088247f5a29369f830e094fffae1b1c
Sebastian Rabuini <sebas@wasabit.com.ar>
2015-01-04 04:41:25 -0300

    Add price providers

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,8 +1,10 @@
+var DEFAULT_CONFIGURATION = {"provider": "bitstamp"};
+
 function fetchData() {
   var response;
   var req = new XMLHttpRequest();
 
-  var provider = new Bitstamp();
+  var provider = getProvider();
   console.log(provider.url);
   req.open('GET', provider.url);
 
@@ -35,44 +37,101 @@ function fetchData() {
   req.send(null);
 }
 
-function BtcE(){
+function BtcE() {
   this.url = "https://btc-e.com/api/3/ticker/btc_usd";
   this.name = "BTC-e";
-  
-  this.data = function(response){
+  this.data = function(response) {
     return {price: response.btc_usd.last, timestamp: response.btc_usd.updated};
   };
 }
 
-function Bitstamp(){
+function Bitstamp() {
   this.url = "https://www.bitstamp.net/api/ticker/";
   this.name = "Bitstamp";
-  
   this.data = function(response) {
     return {price: response.last, timestamp: response.timestamp};
+  };
+}
+
+function Coindesk() {
+  this.url = "http://api.coindesk.com/v1/bpi/currentprice.json";
+  this.name = "Coindesk";
+  this.data = function(response) {
+    return {price: response.bpi.USD.rate_float, timestamp: Date.parse(response.time.updatedISO) / 1000};
   };
 }
 
 function Bitfinex() {
   this.url = "https://api.bitfinex.com/v1/pubticker/btcusd";
   this.name = "Bitfinex";
-  
   this.data = function(response) {
     return {price: response.last_price, timestamp: response.timestamp};
   };
 }
 
-Pebble.addEventListener("ready",
-                        function(e) {
-                          console.log("connect! " + e.ready);
-                          fetchData();
-                          console.log(e.type);
-                        });
+function getConfiguration() {
+  var configuration = localStorage.getItem("configuration");
+  if (configuration) {
+    try {
+      return JSON.parse(configuration);
+    } catch(e) {
+      console.log("WARN: error parsing configuration from localstorage: " + e);
+      return DEFAULT_CONFIGURATION;
+    }
+  } else {
+    return DEFAULT_CONFIGURATION;
+  }
+}
 
-Pebble.addEventListener("appmessage",
-                        function(e) {
-                          fetchData();
-                          console.log(e.type);
-                          console.log(e.payload);
-                          console.log("message!");
-                        });
+function getProvider() {
+  var configuration = getConfiguration();
+  var provider = configuration.provider;
+  var objProvider;
+
+  console.log("Getting provider...");
+  console.log(getConfiguration());
+  console.log(provider);
+
+  switch (provider) {
+    case "bitstamp":
+      objProvider = new Bitstamp();
+      break;
+    case "btc_e":
+      objProvider = new BtcE();
+      break;
+    case "coindesk":
+      objProvider = new Coindesk();
+      break;
+    case "bitfinex":
+      objProvider = new Bitfinex();
+      break;
+    default:
+      objProvider = new Bitstamp();
+  }
+
+  return objProvider;
+}
+
+Pebble.addEventListener("ready", function(e) {
+  console.log("connect! " + e.ready);
+  fetchData();
+  console.log(e.type);
+});
+
+Pebble.addEventListener("appmessage", function(e) {
+  fetchData();
+  console.log(e.type);
+  console.log(e.payload);
+  console.log("message!");
+});
+
+Pebble.addEventListener("showConfiguration", function(e) {
+  Pebble.openURL("http://wasabitlabs.com/en/btc-simple");
+});
+
+Pebble.addEventListener("webviewclosed", function(e) {
+  var configuration = JSON.parse(decodeURIComponent(e.response));
+  localStorage.setItem("configuration", JSON.stringify(configuration));
+  console.log("Configuration window returned: " + JSON.stringify(configuration));
+  fetchData();
+});
